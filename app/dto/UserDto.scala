9dec2c09ec9795428b32e40ec9d81860fe77f108
hex0x0 <sqh1107@gmail.com>
2019-07-23 13:55:13 +0800

    update user dto

--- app/dto/UserDto.scala
+++ app/dto/UserDto.scala
@@ -5,12 +5,11 @@ import play.api.libs.json.{Json, Writes}
 case class UserDto(id: Int, name: String, age: Int, city: String)
 
 object UserDto {
-  implicit val writes: Writes[UserDto] = (u: UserDto) => {
-    Json.obj(
+  implicit val writes: Writes[UserDto] =
+    (u: UserDto) => Json.obj(
       "id" -> u.id,
       "name" -> u.name,
       "age" -> u.age,
       "city" -> u.city
     )
-  }
 }
