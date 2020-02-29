878b03f96fa8e7daa26dbdba8ced60be9d65f713
hex0x0 <sqh1107@gmail.com>
2020-02-29 23:30:29 +0800

    update

--- app/controller/AccountController.scala
+++ app/controller/AccountController.scala
@@ -15,28 +15,28 @@ class AccountController @Inject()(cc: ControllerComponents, userService: Account
   private val logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME)
 
   def all: Action[AnyContent] = Action.async {
-    logger.info("Finding all accounts.")
-    userService.all.map(u => Ok(Json.toJson(u)))
+    logger.info("Try to find all accounts.")
+    userService.all.map(u => Ok(Json.toJson(u)).withHeaders(("Access-Control-Allow-Origin", "*")))
   }
 
   def findById(id: Int): Action[AnyContent] = Action.async {
     userService.findById(id).map {
       case Left(error) => BadRequest(Json.obj("message" -> error))
-      case Right(dto) => Ok(Json.toJson(dto))
+      case Right(dto) => Ok(Json.toJson(dto)).withHeaders(("Access-Control-Allow-Origin", "*"))
     }
   }
 
   def create: Action[JsValue] = Action(parse.json).async { request =>
     request.body.validate[AccountForm].fold(
       error => Future(BadRequest(Json.obj("message" -> error.toString))),
-      form => userService.create(form).map(dto => Ok(Json.toJson(dto)))
+      form => userService.create(form).map(dto => Ok(Json.toJson(dto)).withHeaders(("Access-Control-Allow-Origin", "*")))
     )
   }
 
   def update(id: Int): Action[JsValue] = Action(parse.json).async { request =>
     request.body.validate[AccountForm].fold(
       error => Future(BadRequest(Json.obj("message" -> error.toString))),
-      form => userService.update(id, form).map(result => Ok(Json.toJson(result)))
+      form => userService.update(id, form).map(result => Ok(Json.toJson(result)).withHeaders(("Access-Control-Allow-Origin", "*")))
     )
   }
 
